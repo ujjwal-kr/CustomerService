465e7c128798c8d555fa2bf04da83611fea74c06
Ujjwal <katanadeveloper01@gmail.com>
2019-04-09 16:21:37 +0530

    switched the order of routes to minimise performance hits

--- start/routes.js
+++ start/routes.js
@@ -7,14 +7,14 @@ const Route = use('Route')
 Route.get('customers', 'CustomerController.index')
 Route.post('customers', 'CustomerController.store').middleware(['validate', 'cnull'])
 Route.get('customers/:id', 'CustomerController.show').middleware(['findCustomer'])
-Route.patch('customers/:id', 'CustomerController.update').middleware(['findCustomer', 'validate', 'cnull'])
+Route.patch('customers/:id', 'CustomerController.update').middleware(['validate', 'findCustomer', 'cnull'])
 Route.delete('customers/:id', 'CustomerController.destroy').middleware(['findCustomer'])
 
 // GET Customer's Projects
 Route.get('customers/:id/projects', 'CustomerController.fetchWithProjects').middleware(['findCustomer'])
 
 //Projects
-Route.post('customers/:id/projects', 'ProjectController.store').middleware('findCustomer', 'validate')
+Route.post('customers/:id/projects', 'ProjectController.store').middleware('validate', 'findCustomer')
 Route.patch('projects/:id', 'ProjectController.update').middleware(['validate', 'findProject', 'cnull'])
 Route.delete('projects/:id', 'ProjectController.destroy').middleware(['findProject'])
 
@@ -22,6 +22,6 @@ Route.delete('projects/:id', 'ProjectController.destroy').middleware(['findProje
 Route.get('projects/:id/tasks', 'ProjectController.fetchWithTasks').middleware(['findProject'])
 
 //tasks
-Route.post('projects/:id/tasks', 'TaskController.store').middleware('findProject','validate')
+Route.post('projects/:id/tasks', 'TaskController.store').middleware('validate', 'findProject')
 Route.patch('tasks/:id', 'TaskController.update').middleware(['validate', 'cnull', 'findTask'])
 Route.delete('tasks/:id', 'TaskController.destroy').middleware(['findTask'])
