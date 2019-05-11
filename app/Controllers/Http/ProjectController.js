2f1fb4e8cb54e1039a2ccccc0a20c62d364f1f49
Ujjwal <katanadeveloper01@gmail.com>
2019-05-11 14:28:24 +0530

    Update ProjectController.js

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -83,12 +83,11 @@ class ProjectController {
    */
   async destroy ({ params: { id }, request, response }) {
     const project = await Project.find(id)
-
-        await project.delete()
-        return response.status(201).json({
-          msg: 'Sucessfully deleted project',
-          id
-        })
+      await project.delete()
+      return response.status(201).json({
+        msg: 'Sucessfully deleted project',
+        id
+      })
   }
 }
 
